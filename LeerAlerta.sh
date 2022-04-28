f7d58c23cb77385a1b243fed4b563e8be46d2711
lostkitten <dlopezf@ulasalle.edu.pe>
2022-04-28 00:21:45 -0500

    Diferencia entre alerta de usurario root con el resto

--- LeerAlerta.sh
+++ LeerAlerta.sh
@@ -2,7 +2,19 @@
 # El archivo de alerta se sobreescribirá cuando se cree una nueva alerta
 FILE=/SO/scripts/Alerta/alertas.txt
 if [ -f "$FILE" ]; then
-	echo "El archivo existe"
+	while IFS= read -r line
+	do
+		if [ "$line" != "" ]; then
+			user=$(echo "$line" | cut -c1-1)
+			if [ "$user" == '1' ]; then
+				echo "Es root"
+			else
+				echo "No es root"
+			fi
+		else
+			echo "No hay más alertas"
+		fi
+	done < "$FILE"
 else
 	echo "No  existe el archivo alertas.txt"
 	touch alertas.txt
